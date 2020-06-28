fe2354293962d86a4322fdae6a3fa726f1f8518e
Constantine Makeev <costmkv@gmail.com>
2020-06-28 16:17:31 +0300

    add googleDrive api

---
+++ googleDrive.js
@@ -0,0 +1,21 @@
+const { google } = require('googleapis');
+const { authenticate } = require('@google-cloud/local-auth');
+const path = require('path');
+
+const drive = google.drive('v3');
+
+exports.auth = async() => {
+	const localAuth = await authenticate({
+		keyfilePath: path.join(__dirname, './oauth2.keys.json'),
+		scopes: ['https://www.googleapis.com/auth/drive'],
+	});
+
+	google.options({ auth: localAuth });
+};
+
+exports.getFileStream = async(fileId) => {
+	const response = await drive.files
+		.get({fileId, alt: 'media'}, {responseType: 'stream'});
+
+	return response.data;
+};
--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const db = require('./db');
+const googleDrive = require('./googleDrive');
 
 (async () => {
 	try {
 		const uri = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/testMovies';
 		await db.init(uri);
 
+		await googleDrive.auth();
 	}catch (e) {
 		throw new Error(e);
 	}finally {
